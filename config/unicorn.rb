d2b5df5b5297a5fa8ecf9b07465b26698a54afb9
akihiro tokuhara <muscletown_mcm@yahoo.co.jp>
2020-07-14 22:57:41 +0900

    unicorn.rb修正

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,9 +1,9 @@
-require 'fileutils'
+worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
+timeout 15
 preload_app true
-timeout 5
 worker_processes 4
-listen '/tmp/nginx.socket', backlog: 1024
+listen 'unix:///tmp/nginx.socket', backlog: 1024
 
 before_fork do |server,worker|
-	FileUtils.touch('/tmp/app-initialized')
+    FileUtils.touch('/tmp/app-initialized')
 end
